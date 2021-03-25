1663ee28109dcd9554f28d2285bdccb4ddd2065e
Dawid Dieu <dawid.dieu@gmail.com>
2021-03-25 18:52:38 +0100

    Create data loader (#4)
    * Create Dataset class
    * DataLoader from Dataset

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -1,3 +1,9 @@
+import os
+import torch
+import pandas as pd
+from torchvision import transforms, utils
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 
 
@@ -64,3 +70,63 @@ class InMemDataLoader(object):
     def to(self, device):
         self.dataset.tensors = tuple(t.to(device) for t in self.dataset.tensors)
         return self
+
+
+class PetsDataset(Dataset):
+
+    def __init__(
+        self,
+        root_dir,
+        transform=None
+    ):
+        datapoints_names = list(sorted(os.listdir(f'{root_dir}/datapoints')))
+        label_names = list(sorted(os.listdir(f'{root_dir}/labels')))
+
+        self.dataset = list(zip(datapoints_names, label_names))
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        datapoint_name = self.dataset[idx][0]
+        label_name = self.dataset[idx][1]
+
+        datapoint = Image.open(f'{self.root_dir}/datapoints/{datapoint_name}')
+        label = Image.open(f'{self.root_dir}/labels/{label_name}')
+
+        if self.transform:
+            datapoint = self.transform(datapoint)
+            label = self.transform(label)
+
+        return datapoint, label
+
+
+if __name__ == '__main__':
+    current_dir = os.getcwd()
+
+    dataset = PetsDataset(
+        root_dir=f'{current_dir}/dataset/valid',
+        transform=transforms.Compose([
+            transforms.Resize((240, 240)),
+            transforms.ToTensor(),
+        ])
+    )
+
+    print(len(dataset))
+
+    for x, y in dataset:
+        print(x.shape, y.shape)
+        break
+
+    dataloader = DataLoader(
+        dataset, batch_size=4,
+        shuffle=True, num_workers=0
+    )
+
+    print('\n=============================\n')
+
+    for x, y in dataloader:
+        print(x.shape, y.shape)
+        break
